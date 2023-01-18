402f38ee1bfc3378248ad88f27d671b10d028b32
TRFrankum <123021957+TRFrankum@users.noreply.github.com>
2023-01-18 14:27:49 -0600

    Added comments

--- returnunicode.py
+++ returnunicode.py
@@ -15,18 +15,22 @@ characterenteredstate = 0
 
 #Prompt the user for the character they wish to lookup
 
-
 while (characterenteredstate) < 1:
     lookupcharacter = input("Please enter the character you wish to lookup: \n")
+    #This is to make sure the user only inputs one character.
     if len(lookupcharacter) > 1:
         print('\nPlease only enter one character \n')
     else:
         characterenteredstate = 1
+
+#Lookup and store the results from unicodedata and the built in ord function into strings for later use. 
+
 else:
     charactername = unicodedata.name (lookupcharacter)
     charactercoderaw = ord(lookupcharacter)
     characercodehex = format(charactercoderaw, '#06x')
 
+#Put it all together using the variables we collected and then print it out. 
 
     constructedoutputstring = f"\n{lookupcharacter} is known as {charactername} and has a unicode hexidecimal value of {characercodehex}"
     
